95cd637fd23625801691c5b03af29ae5668f324b
eslessons <66363651+eslessons@users.noreply.github.com>
2020-09-11 08:32:41 +0300

    Start Udemy - Electron From Scratch. Build Desktop Apps With JavaScript -> 1. Introduction

---
+++ JS Related Courses/Udemy - Electron From Scratch. Build Desktop Apps With JavaScript.js
@@ -0,0 +1,17 @@
+/* Udemy 
+Electron From Scratch. Build Desktop Apps With JavaScript
+
+1. Introduction
+Start
+
+
+2. Project 1 - Image Shrink App & Getting Started With Electron
+3. Working With Menu
+4. Interface, IPC, ImageMin & Packaging
+5. Project 2 - SysTop App
+6. System Stats & Renderer Process
+7. Storing Local Data
+8. Tray Icon & Wrap Up
+9. Project 3 - BugLogger App & Creating the React UI
+10. Main Process, IPC & MongoDB
+*/
