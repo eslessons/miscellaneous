2c92a8198896d6a4646df00e26edd003dc0d239e
eslessons <66363651+eslessons@users.noreply.github.com>
2020-09-11 11:04:21 +0300

    Finish 2. Project 1 - Image Shrink App & Getting Started With Electron

--- JS Related Courses/Udemy - Electron From Scratch. Build Desktop Apps With JavaScript.js
+++ JS Related Courses/Udemy - Electron From Scratch. Build Desktop Apps With JavaScript.js
@@ -3,15 +3,20 @@ Electron From Scratch. Build Desktop Apps With JavaScript
 
 1. Introduction
 Start
-Welcome to the course
-Electron what why
-How electron works
-Environment code repo
+	Welcome to the course
+	Electron what why
+	How electron works
+	Environment code repo
 Finish
 
 2. Project 1 - Image Shrink App & Getting Started With Electron
 Start
-
+	Project intro
+	Setup BrowserWindow
+	Loading a window file adding an icon
+	Environment platform check
+	Using nodemon with Electron
+Finish
 
 3. Working With Menu
 4. Interface, IPC, ImageMin & Packaging
