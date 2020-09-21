ba63ef5c43fa5524836108268f0e12728d768aa6
eslessons <eslessons@tutanota.com>
2020-09-21 13:33:15 +0300

    Finish 1. Introduction

--- JS Related Courses/Frontendmasters.com - Introduction to Node.js
+++ JS Related Courses/Frontendmasters.com - Introduction to Node.js
@@ -3,7 +3,16 @@ Introduction to Node.js
 
 1. Introduction
 Start
-
+	1. Course Overview
+	2. What is Node.js
+	3. Creating with Node.js
+	4. Installing Node.js
+	5. Running Node.js Code
+	6. Installing Node.js from the source
+	7. Installing Node.js with NVM
+	8. Install and Run Node.js Exercise
+	9. Checking Install & Running Node.js Solution
+Finish
 
 2. Node.js Modules
 3. Internal Modules & npm
