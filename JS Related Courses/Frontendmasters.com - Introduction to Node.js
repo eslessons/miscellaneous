f6e27095608ba546846eeb7ac1081d4e9ce45c9c
eslessons <eslessons@tutanota.com>
2020-09-21 17:21:13 +0300

    Finish 2. Node.js Modules

--- JS Related Courses/Frontendmasters.com - Introduction to Node.js
+++ JS Related Courses/Frontendmasters.com - Introduction to Node.js
@@ -16,8 +16,15 @@ Finish
 
 2. Node.js Modules
 Start
-
-
+	1. Browser JavaScript vs Node.js
+	2. Globals in Node.js
+	3. Node.js Modules
+	4. Creating Node Modules
+	5. Importing Node Modules
+	6. Q&A- ES Modules & Latest JS Features
+	7. Convert Browser JavaScript Exercise
+	8. Convert Browser JavaScript Solution
+Finish
 
 3. Internal Modules & npm
 4. Asynchronous Node
