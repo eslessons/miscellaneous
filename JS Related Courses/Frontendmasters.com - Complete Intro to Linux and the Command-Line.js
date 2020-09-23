7b1a76aa792f9cff1e6cdb50ae09c080688b510f
eslessons <eslessons@tutanota.com>
2020-09-23 17:01:42 +0300

    Start Frontendmasters.com - Complete Intro to Linux and the Command-Line -> 1. Introduction

---
+++ JS Related Courses/Frontendmasters.com - Complete Intro to Linux and the Command-Line.js
@@ -0,0 +1,17 @@
+/* Frontendmasters.com 
+Complete Intro to Linux and the Command-Line
+
+1. Introduction
+Start
+
+2. The CLI
+3. Editors
+4. Files, Pipes, & Permissions
+5. Environments & Processes
+6. Networking & the Internet
+7. Package Management
+8. Shell Scripts
+9. Automation & Customization
+10. Wrapping Up
+
+*/
