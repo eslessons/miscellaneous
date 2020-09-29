5aa605d6eb778e19af1a3181f4d331aaf243d85f
eslessons <eslessons@tutanota.com>
2020-09-29 09:44:27 +0300

    Start Frontendmasters.com - Complete Intro to Containers -> 1. Introduction

---
+++ JS Related Courses/Frontendmasters.com - Complete Intro to Containers.js
@@ -0,0 +1,16 @@
+/* Frontendmasters.com
+Complete Intro to Containers
+
+1. Introduction
+Start
+
+2. Containers
+3. Docker
+4. The Dockerfile
+5. Making Tiny Containers
+6. Features in Docker
+7. Multi Container Projects
+8. OCI
+9. Wrapping Up
+
+*/
