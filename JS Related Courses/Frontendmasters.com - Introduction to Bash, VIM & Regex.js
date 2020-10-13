5136eeeffefbff41cb56c1018c254d89875eaee7
eslessons <eslessons@tutanota.com>
2020-10-13 11:09:05 +0300

    Finish 2. File Management

--- JS Related Courses/Frontendmasters.com - Introduction to Bash, VIM & Regex.js
+++ JS Related Courses/Frontendmasters.com - Introduction to Bash, VIM & Regex.js
@@ -12,6 +12,14 @@ Finish
 
 2. File Management
 Start
+	1. Combining and Copying Files
+	2. Moving Files and Creating Directories
+	3. Commands to Help Reduce Repetitive Typing
+	4. Deleting Files and Counting Words
+	5. Documentation and Options
+	6. Paths
+	7. Writing and Reading Files
+Finish
 
 3. Pipes
 4. Bash Shell Scripts
