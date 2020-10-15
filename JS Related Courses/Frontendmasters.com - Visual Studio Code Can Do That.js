2918fcf79d98fb8220d750dc0d8d739d26dc26f4
eslessons <eslessons@tutanota.com>
2020-10-15 09:05:13 +0300

    Start Frontendmasters.com - Visual Studio Code Can Do That -> 1. Introduction

---
+++ JS Related Courses/Frontendmasters.com - Visual Studio Code Can Do That.js
@@ -0,0 +1,16 @@
+/* Frontendmasters.com 
+Visual Studio Code Can Do That
+
+1. Introduction
+Start
+
+2. Customizing the Editor
+3. Productivity Tricks
+4. Navigation & Refactoring
+5. Debugging
+6. Docker
+7. Remote Development
+8. Working with Data
+9. Git
+
+*/
