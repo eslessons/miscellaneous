d7893572b5f65b5e418cec9c45a7def1e4f4ec14
eslessons <eslessons@tutanota.com>
2020-10-19 09:15:20 +0300

    Start Frontendmasters.com - Visual Studio Code -> 1. Visual Studio Code

---
+++ JS Related Courses/Frontendmasters.com - Visual Studio Code.js
@@ -0,0 +1,16 @@
+/* Frontendmasters.com
+Visual Studio Code
+
+1. Visual Studio Code
+Start
+
+2. Documents
+3. Code Navigation & Refactoring
+4. Type Checking
+5. Debugging
+6. Workspace Customization
+7. Tasks
+8. Launch Configuration
+9. Wrapping Up VS Code
+
+*/
