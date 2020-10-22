54f1d5b41743732df9e7b7f64a410273672546d5
eslessons <eslessons@tutanota.com>
2020-10-22 18:52:56 +0300

    Finish 3. ESLint Plugin And AST

--- JS Related Courses/Frontendmasters.com - Code Transformation and Linting with ASTs.js
+++ JS Related Courses/Frontendmasters.com - Code Transformation and Linting with ASTs.js
@@ -15,6 +15,17 @@ Finish
 
 3. ESLint Plugin And AST
 Start
+	1. Challenge 1 - Making a Plugin
+	2. Challenge 1 - Solution
+	3. Challenge 2 - Adding New Methods
+	4. Challenge 2 - Solution
+	5. Challenge 3 - Schema Property
+	6. Challenge 3 - Solution
+	7. Challenge 4 - Edge  Cases
+	8. Challenge 4 - Solution
+	9. Challenge 5 - Applying Fixes
+	10. Challenge 5 - Solution
+Finish
 
 4. Babel Plugin and AST
 5. Codemods and AST
