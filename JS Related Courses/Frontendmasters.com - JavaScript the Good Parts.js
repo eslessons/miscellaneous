76a758a8dbf3d143512917a42fe034f9e1e121a0
eslessons <eslessons@tutanota.com>
2020-10-26 10:26:31 +0300

    Start Frontendmasters.com - JavaScript the Good Parts -> 01. Introduction

---
+++ JS Related Courses/Frontendmasters.com - JavaScript the Good Parts.js
@@ -0,0 +1,13 @@
+/* Frontendmasters.com 
+JavaScript the Good Parts
+
+01. Introduction
+Start
+
+02. Programming Style & Your Brain
+03. And Then There Was JavaScript
+04. Function the Ultimate
+05. Problems
+06. Monads & Gonads
+
+*/
