747c80d15dac8d2027c59d8a4379c0d68d3d5888
eslessons <eslessons@tutanota.com>
2020-10-26 12:59:45 +0300

    Finish 02. Programming Style & Your Brain

--- JS Related Courses/Frontendmasters.com - JavaScript the Good Parts.js
+++ JS Related Courses/Frontendmasters.com - JavaScript the Good Parts.js
@@ -8,6 +8,14 @@ Finish
 
 02. Programming Style & Your Brain
 Start
+	01. The Way We Think
+	02. The Programming Thought Process
+	03. JavaScript  Good Parts and Bad Parts
+	04. Understanding Good Programming Style
+	05. Avoiding Confusing Code (A)
+	06. Avoiding Confusing Code (B)
+	07. Using JSLint
+Finish
 
 03. And Then There Was JavaScript
 04. Function the Ultimate
